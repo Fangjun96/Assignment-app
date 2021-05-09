a2877c90d0826b0652537b1b676e0ac18b6ca106
ngo <fangjunngo@gmail.com>
2021-05-09 23:48:51 +0800

    My third commit

--- Back-end/app.js
+++ Back-end/app.js
@@ -2,9 +2,8 @@ const dotenv = require('dotenv')
 dotenv.config({path: '/.env'})
 const express = require("express")
 const mongoose = require('mongoose')
-
 const app = express()
-console.log(require('dotenv').config())
+
 mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology:true})
 //
 app.use('/api', require('./routes'))
